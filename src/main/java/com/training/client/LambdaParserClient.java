07b062509ca1d549bcaa4869ab9fae0a2a5aef7b
Jhefrey Sajot <jsajot@fullscale.ph>
2026-08-19 14:41:37 +0800

    added sample properties config, added @ClientFitler for declarative client and remove hardcoded service name log

--- src/main/java/com/training/client/LambdaParserClient.java
+++ src/main/java/com/training/client/LambdaParserClient.java
@@ -4,12 +4,14 @@ import com.training.data.request.DocumentParserRequest;
 import com.training.data.result.ParsedResult;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.ClientFilter;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.retry.annotation.Recoverable;
 import io.micronaut.retry.annotation.Retryable;
 import reactor.core.publisher.Mono;
 
+@ClientFilter("/**")
 @Client(value = "${lambda.parser.url}", id = "document-parser")
 public interface LambdaParserClient {
 
